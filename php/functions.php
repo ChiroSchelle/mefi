e5fe9332265f3167feac42ec65c8c6fcb00eff71
Jonas Morel <jonas.morel@chiroschelle.be>
2018-05-08 20:29:45 +0200

    Wanneer LactoseX niet zichtbaar is andere waarde geven.

--- php/functions.php
+++ php/functions.php
@@ -13,7 +13,13 @@
 
 		if($_POST["function"] == "NieuweFiche")
 		{
-			NieuweFiche($_POST["Lactose"],$_POST["LactoseX"],$_POST["ToestemmingFotoS"],$_POST["ToestemmingFoto"],$_POST["Afdeling"],$_POST["Naam"],$_POST["Voornaam"],$_POST["Geboortedatum"],$_POST["Straat"],$_POST["Nummer"],$_POST["Postcode"],$_POST["Woonplaats"],$_POST["Telefoon1"],$_POST["Telefoon2"],$_POST["NaamVoornaam1"],$_POST["NaamVoornaam2"],$_POST["Verwantschap1"],$_POST["Verwantschap2"],$_POST["NaamHuisarts"],$_POST["TelefoonArts"],$_POST["Bloedgroep"],$_POST["ResusFactor"],$_POST["ZiektenLijst"],$_POST["GevoeligStoffenVoeding"],$_POST["Ingrepen"],$_POST["VaccinatieTetanus"],$_POST["SerumTetanus"],$_POST["VaccinatieHersenvliesontsteking"],$_POST["AllergieMedicatie"],$_POST["Vegetarisch"],$_POST["Incontinent"],$_POST["DeelnemenSport"],$_POST["ToestemmingMedicatie"],$_POST["ToestemmingIngrepen"],$_POST["AndereInlichtingen"],$_POST["NaamInvuller"],$_POST["Roken"],$_POST["ToestemmingRoken"],$_POST["MedicatieLijst"]);
+			if ($_POST["LactoseX"] == null){
+				$LactoseX = "/";
+			}
+			else {
+				$LactoseX = $_POST["LactoseX"];
+			}
+			NieuweFiche($_POST["Lactose"],$Lactose,$_POST["ToestemmingFotoS"],$_POST["ToestemmingFoto"],$_POST["Afdeling"],$_POST["Naam"],$_POST["Voornaam"],$_POST["Geboortedatum"],$_POST["Straat"],$_POST["Nummer"],$_POST["Postcode"],$_POST["Woonplaats"],$_POST["Telefoon1"],$_POST["Telefoon2"],$_POST["NaamVoornaam1"],$_POST["NaamVoornaam2"],$_POST["Verwantschap1"],$_POST["Verwantschap2"],$_POST["NaamHuisarts"],$_POST["TelefoonArts"],$_POST["Bloedgroep"],$_POST["ResusFactor"],$_POST["ZiektenLijst"],$_POST["GevoeligStoffenVoeding"],$_POST["Ingrepen"],$_POST["VaccinatieTetanus"],$_POST["SerumTetanus"],$_POST["VaccinatieHersenvliesontsteking"],$_POST["AllergieMedicatie"],$_POST["Vegetarisch"],$_POST["Incontinent"],$_POST["DeelnemenSport"],$_POST["ToestemmingMedicatie"],$_POST["ToestemmingIngrepen"],$_POST["AndereInlichtingen"],$_POST["NaamInvuller"],$_POST["Roken"],$_POST["ToestemmingRoken"],$_POST["MedicatieLijst"]);
 		}
 	}
 
